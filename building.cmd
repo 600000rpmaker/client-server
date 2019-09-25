9601ea6eb80480dc2f67e2a46e98528d2bb84dc0
600000rpm <55796427+600000rpm@users.noreply.github.com>
2019-09-25 16:33:30 +0300

    Update building.cmd

--- building.cmd
+++ building.cmd
@@ -1,7 +1,6 @@
 rem Скрипт требует наличие предустановленного python2 и компилятора visual studio
 echo "Building exe from py-scripts"
-"C:\Python27\Scripts\pyinstaller" -D -F .\ntrip_server.py
-"C:\Python27\Scripts\pyinstaller" -D -F .\ntrip_caster.py
+"C:\Python27\Scripts\pyinstaller" -D -F .\*.py
 rem Получаем текущую директорию в переменную
 FOR /F "usebackq tokens=*" %%i IN (`cd`) DO set CurrentDir=%%i
 move %CurrentDir%\dist\*.exe %CurrentDir%\
